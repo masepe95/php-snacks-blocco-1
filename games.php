eb67343764fe9b4951277ffdf63b7fa2393ec459
masepe95 <marcosepe10@gmail.com>
2023-07-17 23:02:39 +0200

    initial commit

---
+++ games.php
@@ -0,0 +1,37 @@
+<?php
+
+$matches = [
+    [
+        'home' => [
+            'name' => 'JuveCaserta',
+            'points' => 83
+        ],
+        'guest' => [
+
+            'name' => 'Olimpia Milano',
+            'points' => 71
+        ]
+    ],
+    [
+        'home' => [
+            'name' => 'Poggibonsi',
+            'points' => 59
+        ],
+        'guest' => [
+
+            'name' => 'Cantù',
+            'points' => 61
+        ]
+    ],
+    [
+        'home' => [
+            'name' => 'Longobarda',
+            'points' => 42
+        ],
+        'guest' => [
+
+            'name' => 'OronzoCanà',
+            'points' => 90
+        ]
+    ],
+];
